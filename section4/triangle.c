73ff50160743137fb4fc7433674631c5b9ce0d5d
k-azuki-t <135600349+k-azuki-t@users.noreply.github.com>
2025-06-23 21:35:34 +0900

    first commit

---
+++ section4/triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float base, height;
+    float area;
+
+    base = 3.0;
+    height = 4.0;
+
+    area = (base * height) / 2.0;
+
+    printf("triangle area %f\n", area);
+
+    return 0;
+}
